ff6d472ea32732b3392de095e495d4a5cab59a03
quangtv <trinhvanquangf1@gmail.com>
2024-04-19 09:26:49 +0000

    add custom tools

---
+++ custom_tools/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Custom Tools',
+    'version': '0.1',
+    'summary': 'Custom tools',
+    'sequence': 9999,
+    'category': 'Hidden',
+    'description': """ 
+    Custom tools
+    """,
+    'data': [
+    ],
+    'depends': [
+        'mail',
+    ],
+    'auto_install': False,
+    'license': '',
+}
