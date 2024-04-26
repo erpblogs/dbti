9667346b79c12830d06d27dda4e430201824288f
newlifeoftu <newlifeoftu92@gmail.com>
2024-04-26 15:04:25 +0700

    [17][IMP][hr_org_chart] change name org chart

---
+++ custom_hr_employee_tree/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Custom Hr Employee Tree",
+    'summary': "Change name menu items module hr",
+    'author': "Nguyentu",
+    'website': "savvycom",
+    'category': 'Hidden',
+    'version': '1.0.1',
+    
+    'depends': ['base', 'hr_org_chart'],
+    'data': [
+        'views/custom_hr_employee_tree_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
