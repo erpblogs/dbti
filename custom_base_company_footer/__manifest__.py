e2cf4ba41b290a99eafce62067af1d48faf28337
Nguyễn Nguyên Tú <104961549+newlifeoftu@users.noreply.github.com>
2024-04-30 09:57:46 +0700

    Update __manifest__.py
    If setup module invoice ,it will display footer "Company create" screen

--- custom_base_company_footer/__manifest__.py
+++ custom_base_company_footer/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-
+# If setup module invoice , it will display footer in "Company create" screen
 {
     'name': 'Custom Base Company Footer',
     'version': '0.1',
