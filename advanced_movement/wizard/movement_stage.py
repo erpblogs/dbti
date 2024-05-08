5c1e5eb85b24623effaa3a915a5808bee0d2787f
newlifeoftu <newlifeoftu92@gmail.com>
2024-05-08 17:08:51 +0700

    [17][IMP] edit fiels and changes field

--- advanced_movement/wizard/movement_stage.py
+++ advanced_movement/wizard/movement_stage.py
@@ -5,11 +5,11 @@ class Stage(models.Model):
     _name = "movement.stage"
     _description = " Movement Stage "
     _order = "sequence"
-    
-    name = fields.Char(string="Stage Name", size=100, required=True)
-    approval_role_id = fields.Many2one("approval.role", string="Approval Type", required=True)
+
+    approval_role_id = fields.Many2one("approval.role", string="Stage Name", required=True)
+    approval_type = fields.Char(string="Approval Type", size=100, required=True)
     approvers = fields.Many2one('hr.employee', string="Approvers(Predefined)", required=True)
-    sequence = fields.Integer(string="Sequence")
+    sequence = fields.Char(string="Sequence")
     approval_flow_id = fields.Many2one("approval.flow", string="Approval Flow")
 
     def unlink(self):
